cb6c5439130b05d6ab5405a28ca99143b213fb85
Mohit Patel <patelmohit719@gmail.com>
2021-05-13 23:10:13 +0530

    project setup

---
+++ README.md
@@ -0,0 +1,16 @@
+iBox, where you can showcase your best moment to the world.
+
+# Technology
+- React
+- Firebase
+
+# Deploy
+- url
+
+# Run local
+git clone url
+cd iBox
+
+- run
+npm install
+npm start
---
+++ src/components/Title.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Title = () => {
+  return (
+    <div className="title">
+      <h2>iBox</h2>
+      <p>Share your moments here!</p>
+    </div>
+  )
+}
+
+export default Title;
